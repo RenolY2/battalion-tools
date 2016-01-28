6ce6f22837b9e6a1dc8423038b6e2eb3d0a8de89
Yoshi2 <debugyoshi@gmail.com>
2016-01-28 21:28:43 +0100

    Add big endian integer reading

--- rxet/helper.py
+++ rxet/helper.py
@@ -3,3 +3,7 @@ from struct import unpack
 
 def read_uint32(fileobj):
     return unpack("I", fileobj.read(4))[0]
+
+# read int as a big endian number
+def read_uint32_BE(fileobj):
+    return unpack(">I", fileobj.read(4))[0]
